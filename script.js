b78a83fc28da13ab5bd017271a42b006f5f60080
Luan Gomes Galvão <luanitachi@hotmail.com>
2023-03-04 22:04:06 -0300

    add API price

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const valorTwo = document.querySelector(".valorTwo");
 const select = document.querySelector(".select");
 const secondImg = document.querySelector(".secondImg");
 let convertValue 
+const API = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL"
 btn.addEventListener("click", convert)
 select.addEventListener("change", chandeCurrency)
 
@@ -17,27 +18,38 @@ function convert() {
 
 function chandeCurrency() {
     const money = document.querySelectorAll(".money");
+    const price = fetch(API).then((response) => {return response.json()}).catch((error) => {console.log(error)})
+    price.then((data) => {
+
+        switch (select.value) {
+            case "Dolar":
+                secondImg.src = "/img/estados-unidos (1) 1.svg"
+                secondImg.alt = "EUA";
+                money[1].innerHTML = "Dólar Americano"
+                convertValue = input.value / data.USDBRL.ask;
+                valorTwo.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertValue);
+                return convertValue
+                break;
+            case "Euro":
+                secondImg.src = "/img/euro.svg"
+                secondImg.alt = "Europa";
+                money[1].innerHTML = "Euro"
+                convertValue = input.value / data.EURBRL.ask
+                valorTwo.innerHTML = new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(convertValue);
+                return convertValue
+                break;
+            default:
+                break;
+        }
+           
+    })
     
-    switch (select.value) {
-        case "Dolar":
-            secondImg.src = "/img/estados-unidos (1) 1.svg"
-            secondImg.alt = "EUA";
-            money[1].innerHTML = "Dólar Americano"
-            convertValue = input.value / 5.20;
-            valorTwo.innerHTML = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"}).format(convertValue);
-            return convertValue
-            break;
-        case "Euro":
-            secondImg.src = "/img/euro.svg"
-            secondImg.alt = "Europa";
-            money[1].innerHTML = "Euro"
-            convertValue = input.value / 5.53
-            valorTwo.innerHTML = new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(convertValue);
-            return convertValue
-            break;
-        default:
-            break;
-    }
     
 }
+
+
+
+
+
+
 
